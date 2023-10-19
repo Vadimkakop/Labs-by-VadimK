9defa579b75d633afebe0b5014c5fb83cc9e6e05
Vadimkakop <147658913+Vadimkakop@users.noreply.github.com>
2023-10-19 20:51:33 +0300

    Update task_2.py

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -8,5 +8,5 @@ size_of_flop_in_kb = size_of_flop_in_mb * 1024
 size_of_flop_in_b = size_of_flop_in_kb * 1024
 numbers_of_summary_symbols = numbers_of_symbols * numbers_of_str * numbers_of_pages
 size_of_summary_symbols = numbers_of_summary_symbols * size_of_symbols_in_b
-numbers_of_books = size_of_flop_in_b / size_of_summary_symbols
-print("Количество книг, помещающихся на дискету:", round(numbers_of_books))
+numbers_of_books = size_of_flop_in_b // size_of_summary_symbols
+print("Количество книг, помещающихся на дискету:", numbers_of_books)
