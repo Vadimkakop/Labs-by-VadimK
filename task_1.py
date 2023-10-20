29e60a8e4e4efbacb01c8c2cb6c8536e1e0d2c4b
Vadimkakop <147658913+Vadimkakop@users.noreply.github.com>
2023-10-20 22:47:28 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,8 +2,8 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-balance = money_capital + salary  # Начальный остаток средств
-count = 0
+balance = money_capital  # Начальный остаток средств
+count = 1  # Месяц работаем до первой зарплаты и первой не повышенной траты
 
 while balance > spend:
     balance += salary - spend
